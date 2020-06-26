d36f78422ae65de027430b35af88a256c7783afb
Duncan Sutter <dsutter@qualivera.com>
2020-06-26 16:33:58 -0400

    Add a webform hack

--- src/Controller/AwsformController.php
+++ src/Controller/AwsformController.php
@@ -7,6 +7,9 @@ use Drupal\Core\Render\HtmlResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Aws\Rekognition\RekognitionClient;
+use Drupal\webform\Entity\Webform;
+use Drupal\webform\Entity\WebformSubmission;
+use Drupal\webform\WebformSubmissionForm;
 
 class AwsformController extends ControllerBase {
   function apitest() {
@@ -16,7 +19,7 @@ class AwsformController extends ControllerBase {
     $image = 'olive.jpg';
 
     // Export aws credentials
- 
+
     putenv("AWS_ACCESS_KEY_ID=xxx");
     putenv("AWS_SECRET_ACCESS_KEY=xxx");
     putenv("AWS_SESSION_TOKEN=xxx");
@@ -44,10 +47,46 @@ class AwsformController extends ControllerBase {
       . PHP_EOL
       . 'Confidence: ' . $result['Labels'][$n]['Confidence']
       . PHP_EOL . PHP_EOL;
-   
+    }
+
+    return new HtmlResponse("<pre>$content</pre>");
   }
+
+  function webform() {
+    $sid = $_GET['sid'];
+
+    $content = "ok";
+
+    // Example IDs
+    $webform_id = 'image_recognition';
+    $webform_submission_id = $sid;
+
+    // Check webform is open.
+    $webform = Webform::load($webform_id);
+    $is_open = WebformSubmissionForm::isOpen($webform);
+
+    if ($is_open === TRUE) {
+      // Load submission
+      $webform_submission = WebformSubmission::load($webform_submission_id);
+      $content .= "type=" . get_class($webform_submission);
+
+      // Modify submission values
+      $webform_submission->setElementData('name', 'Whatever');
+
+      // Validate submission.
+      $errors = WebformSubmissionForm::validateWebformSubmission($webform_submission);
+
+      // Check there are no validation errors.
+      if (!empty($errors)) {
+        print_r($errors);
+      }
+      else {
+        // Submit values and get submission ID.
+        $webform_submission = WebformSubmissionForm::submitWebformSubmission($webform_submission);
+//        print $webform_submission->id();
+      }
+    }
+
     return new HtmlResponse("<pre>$content</pre>");
   }
 }
-
-?>
