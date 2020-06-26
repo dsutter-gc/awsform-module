0b1f025b6a4c68a785e5ca118fcaa4684733806b
sammytripp <samtripp5@gmail.com>
2020-06-26 11:32:51 -0400

    Use Rekognition to detect labels of image uploaded to S3 bucket

--- src/Controller/AwsformController.php
+++ src/Controller/AwsformController.php
@@ -6,14 +6,47 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Render\HtmlResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Aws\Rekognition\RekognitionClient;
 
 class AwsformController extends ControllerBase {
   function apitest() {
     $module_path = drupal_get_path('module', 'awsform');
 
-    $content  = "Ready.\n";
-    $content .= "Module path is $module_path";
+    $bucket = 'drupal-rekognition';
+    $image = 'olive.jpg';
 
+    // Export aws credentials
+    putenv("AWS_ACCESS_KEY_ID=xxx");
+    putenv("AWS_SECRET_ACCESS_KEY=xxx");
+    putenv("AWS_SESSION_TOKEN=xxx");
+
+    $rekognition = new RekognitionClient([
+        'region' => 'us-east-1',
+        'version' => 'latest'
+    ]);
+
+     // Call DetectLabels
+     $result = $rekognition->DetectLabels(array(
+      'Image' => array(
+         'S3Object' => array(
+             'Bucket' => $bucket,
+             'Name' => $image
+         )
+      ),
+      'Attributes' => array('ALL')
+      )
+   );
+
+   $content = 'Results: ' . PHP_EOL;
+   for ($n=0;$n<sizeof($result['Labels']); $n++){
+      $content .= 'Label: ' . $result['Labels'][$n]['Name']
+      . PHP_EOL
+      . 'Confidence: ' . $result['Labels'][$n]['Confidence']
+      . PHP_EOL . PHP_EOL;
+   
+  }
     return new HtmlResponse("<pre>$content</pre>");
   }
 }
+
+?>
